5bc7b3e9ba5acfe0459e38b12901366973c79c1c
richielee <Richie@Richies-MacBook-Pro.local>
2017-05-06 14:47:42 +0200

    Karatsuba

--- Karatsuba.py
+++ Karatsuba.py
@@ -1,59 +1,29 @@
 '''
 A multiplication function that implements the Karatsuba Algorithm
-As we aim to process very large numbers, the output should be of type string.
 '''
 
-def strplus(x, y):
-    ans = ''
-    n = max(len(x),len(y))
-    x = '0' * (n - len(x)) + x
-    y = '0' * (n - len(y)) + y
-    x = x[::-1]
-    y = y[::-1]
-    i = 0
-    carry = 0
-    while (i < n):
-        tmp = int(x[i]) + int(y[i]) + carry
-        if (tmp > 9):
-            tmp -= 10
-            carry = 1
-        else:
-            carry = 0
-        ans += str(tmp)
-        i += 1
-    if (carry):
-        ans += str(carry)
-    return (ans[::-1])
-
-def strminus(x, y):
-    pass
-
-def check_plus():
-    ans = 1
-    for i in range(510):
-        for j in range(510):
-            ans *= (i + j == int(strplus(str(i),str(j))))
-    return (ans)
-print (check_plus())
-
 def Karatsuba(x, y):
-    n = len(x)
-    if (n == 2):
-        return (str(int(x) * int(y)))
-    a = x[:n/2]
-    b = x[n/2:]
-    c = y[:n/2]
-    d = y[n/2:]
+    if (len(str(x)) == 1 or len(str(y)) == 1):
+        return (x * y)
+    n = max(len(str(x)), len(str(y)))
+    n_half = n/2
+    a = x / (10 ** n_half)
+    b = x % (10 ** n_half)
+    c = y / (10 ** n_half)
+    d = y % (10 ** n_half)
     ac = Karatsuba(a,c)
     bd = Karatsuba(b,d)
-    a_b = strplus(a, b)
-    c_d = strplus(c, d)
-    abcd = Karatsuba(a_b, c_d)
-    mid = strminus(strminus(abcd, ac), bd)
-    print('abcd',abcd)
-    print('ac', ac)
-    print('bd',bd)
-    return (strplus(strplus((ac + '0' * n) , (mid + '0' * (n/2))), bd))
+    mid = Karatsuba(a + b, c + d)  - ac - bd
+    return (ac * (10 ** (2 * n_half)) + (mid * (10 ** n_half)) + bd)
 
+def check_multiplication():
+    ans = 1
+    for i in range(51):
+        for j in range(51):
+            ans *= (i * j == Karatsuba(i, j))
+    return (ans)
 
+print (Karatsuba(3141592653589793238462643383279502884197169399375105820974944592,\
+2718281828459045235360287471352662497757247093699959574966967627))
+print (check_multiplication())
 
