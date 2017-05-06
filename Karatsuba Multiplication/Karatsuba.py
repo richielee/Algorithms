d4567f19d7998a365c8f0a032af98d751dfbf40a
richielee <Richie@Richies-MacBook-Pro.local>
2017-05-06 17:05:52 +0200

    Algo

--- Karatsuba Multiplication/Karatsuba.py
+++ Karatsuba Multiplication/Karatsuba.py
@@ -17,11 +17,11 @@ def Karatsuba(x, y):
     return (ac * (10 ** (2 * n_half)) + (mid * (10 ** n_half)) + bd)
 
 def check_multiplication():
-    ans = 1
     for i in range(51):
         for j in range(51):
-            ans *= (i * j == Karatsuba(i, j))
-    return (ans)
+            if (i * j != Karatsuba(i, j)):
+                return (0)
+    return (1)
 
 print (Karatsuba(3141592653589793238462643383279502884197169399375105820974944592,\
 2718281828459045235360287471352662497757247093699959574966967627))
